cbe60c772202bc507b65e263e7520c12990d0b33
Micah Dougall <micah.dougall@madetech.com>
2025-08-06 15:58:46 +0100

    [931] Additional TF base setup

--- terraform/pipeline/variables.tf
+++ terraform/pipeline/variables.tf
@@ -1,3 +1,18 @@
+variable "aws_access_key" {
+  description = "Access key for AWS - find this in IAM"
+  type        = string
+  sensitive   = true
+}
+variable "aws_secret_key" {
+  description = "Secret key for AWS - displayed when IAM user is created"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  default = "eu-west-2"
+}
+
 variable "github_repository_url" {
   description = "URL of the GitHub repository containing the machine learning motebooks"
   type        = string
