b9ab37ac0f879ed620667dd6985b1195931171a9
Micah Dougall <micah.dougall@madetech.com>
2025-08-08 12:41:11 +0100

    [931] Simplify Terraform module and include terraform s3 and lock db in IaC

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -1,18 +1,3 @@
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "sfc-models-terraform-state"
-    key            = "statefiles/workspace=default/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "models-terraform-locks"
-    encrypt        = true
-  }
-}
 
 locals {
   workspace_prefix           = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
@@ -20,3 +5,53 @@ locals {
 }
 
 data "aws_caller_identity" "current" {}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sfc-models-terraform-state"
+    key            = "statefiles/workspace=default/backend.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "models-terraform-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "sfc-models-terraform-state"
+
+  tags = {
+    Repo = "MachineLearningModels"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "models-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- terraform/pipeline/outputs.tf
+++ terraform/pipeline/outputs.tf
@@ -9,3 +9,13 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
