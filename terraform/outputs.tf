f2fd22e2d600cebc2af923ff61daefd0e638305f
Micah Dougall <micah.dougall@madetech.com>
2025-08-08 14:44:16 +0100

    [931] Simplify Terraform further using manually created backend S3 and locks db

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,13 +9,3 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_state_lock.name
-  description = "The name of the DynamoDB table"
-}
