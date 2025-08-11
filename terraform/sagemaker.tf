71fc0e39d7c94b49c3900c2289f6c44fcbf97bb3
Micah Dougall <micah.dougall@madetech.com>
2025-08-11 17:08:45 +0100

    [931] Separate main and default sagemaker instances to protect resources

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,5 +1,7 @@
+
+# PROD (main) resources
 resource "aws_sagemaker_notebook_instance" "models_notebook" {
-  name                  = "${local.workspace_prefix}-models-notebook"
+  name                  = "main-models-notebook"
   role_arn              = aws_iam_role.sagemaker_execution_role.arn
   instance_type         = "ml.t3.medium"
   volume_size           = 5
@@ -9,21 +11,21 @@ resource "aws_sagemaker_notebook_instance" "models_notebook" {
 }
 
 resource "aws_sagemaker_code_repository" "models_repo" {
-  code_repository_name = "${local.workspace_prefix}-models-github-repository"
+  code_repository_name = "main-models-github-repository"
 
   git_config {
     repository_url = var.github_repository_url
-    branch         = local.workspace_prefix
+    branch         = "main"
   }
 }
 
 resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "models_lifecycle" {
-  name     = "${local.workspace_prefix}-models-lifecycle"
-  on_start = base64encode(templatefile("scripts/notebooks-on-start.sh", { workspace_prefix = "${local.workspace_prefix}" }))
+  name     = "main-models-lifecycle"
+  on_start = filebase64("scripts/notebooks-on-start.sh")
 }
 
 resource "aws_iam_role" "sagemaker_execution_role" {
-  name = "${local.workspace_prefix}-sagemaker-execution-role"
+  name = "main-sagemaker-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -37,13 +39,64 @@ resource "aws_iam_role" "sagemaker_execution_role" {
   })
 }
 
-# may need more frorm here: https://docs.aws.amazon.com/sagemaker/latest/dg/sagemaker-roles.html#sagemaker-roles-createnotebookinstance-perms
 resource "aws_iam_policy" "sagemaker_full_access_policy" {
-  name   = "${local.workspace_prefix}-sagemaker-full-access-policy"
-  policy = templatefile("policy-documents/sagemaker-notebooks.json", { account_id = data.aws_caller_identity.current.account_id })
+  name   = "main-sagemaker-full-access-policy"
+  policy = templatefile("policy-documents/sagemaker-notebooks-prod.json", { account_id = data.aws_caller_identity.current.account_id })
 }
 
 resource "aws_iam_role_policy_attachment" "sagemaker_full_access" {
   role       = aws_iam_role.sagemaker_execution_role.name
   policy_arn = aws_iam_policy.sagemaker_full_access_policy.arn
 }
+
+
+# DEV (default) resources
+resource "aws_sagemaker_notebook_instance" "models_notebook" {
+  name                  = "default-models-notebook"
+  role_arn              = aws_iam_role.sagemaker_execution_role.arn
+  instance_type         = "ml.t3.medium"
+  volume_size           = 5
+  lifecycle_config_name = aws_sagemaker_notebook_instance_lifecycle_configuration.models_lifecycle.name
+
+  default_code_repository = aws_sagemaker_code_repository.models_repo.id
+}
+
+resource "aws_sagemaker_code_repository" "models_repo" {
+  code_repository_name = "default-models-github-repository"
+
+  git_config {
+    repository_url = var.github_repository_url
+    branch         = local.workspace_prefix
+  }
+}
+
+resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "models_lifecycle" {
+  name     = "default-models-lifecycle"
+  on_start = filebase64("scripts/notebooks-on-start.sh")
+}
+
+resource "aws_iam_role" "sagemaker_execution_role" {
+  name = "default-sagemaker-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "sagemaker.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "sagemaker_full_access_policy" {
+  name   = "default-sagemaker-full-access-policy"
+  policy = templatefile("policy-documents/sagemaker-notebooks-dev.json", { account_id = data.aws_caller_identity.current.account_id })
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_full_access" {
+  role       = aws_iam_role.sagemaker_execution_role.name
+  policy_arn = aws_iam_policy.sagemaker_full_access_policy.arn
+}
+
