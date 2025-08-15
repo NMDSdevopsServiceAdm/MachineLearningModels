52913d47aecaa08548faf677b8ff5d701e270bd2
mulvj-mt <joe.mulvey@madetech.com>
2025-08-15 07:46:46 +0100

    [931] Modify IAM policy structure

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -15,14 +15,20 @@ resource "aws_iam_role" "sagemaker_execution_role" {
   })
 }
 
-resource "aws_iam_policy" "sagemaker_full_access_policy" {
+resource "aws_iam_policy" "additional_access_policy" {
   for_each = var.env
-  name     = "${each.key}-sagemaker-full-access-policy"
+  name     = "${each.key}-sagemaker-additional-access-policy"
   policy   = templatefile("policy-documents/sagemaker-notebooks-${each.key}.json", { account_id = data.aws_caller_identity.current.account_id })
 }
 
 resource "aws_iam_role_policy_attachment" "sagemaker_full_access" {
   for_each   = var.env
   role       = aws_iam_role.sagemaker_execution_role[each.key].name
-  policy_arn = aws_iam_policy.sagemaker_full_access_policy[each.key].arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_additional_access" {
+  for_each   = var.env
+  role       = aws_iam_role.sagemaker_execution_role[each.key].name
+  policy_arn = aws_iam_policy.additional_access_policy[each.key].arn
 }
