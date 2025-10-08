1885817d626f990ff2f5804c0d44cba39680c5e2
mulvj-mt <joe.mulvey@madetech.com>
2025-10-08 15:19:17 +0100

    Reconfigure backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,10 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Bucket defined in ../*.s3.tfbackend
-    key            = "terraform/statefiles/machine-learning-models/backend.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "models-terraform-locks"
-    encrypt        = true
+    key          = "terraform/statefiles/machine-learning-models/backend.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
